d4530b635db8ddaff0e54bb98b41366a78080645
masahiro <mkiji@coe.edu>
2019-04-28 00:23:33 -0500

    Second Commit

--- src/LLController.java
+++ src/LLController.java
@@ -1,31 +1,76 @@
 import javax.swing.*;
+import javax.swing.plaf.metal.MetalTheme;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 
-public class LLController extends JPanel {
+public class LLController extends JPanel{
 
     private LLView view;
     private LLModel model;
     private int keyCode;
     private ArrayList<Mountain> ms;
     private MovingDot mdot;
+    private int safeZone;
 
     public LLController(){
+        setFocusable(true);
+        addKeyListener(new MyKeyAdapter());
         view = new LLView();
         model = new LLModel();
         setPreferredSize(new Dimension(500,500));
+        safeZone = (int)(Math.random() * 400);
         int x = (int)(Math.random() * 450) + 400;
         int y = (int)(Math.random() * 300) + 100;
-        mdot = new MovingDot(new Point(250,30), new Point(x, y), 6 );
-        MyKeyAdapter myKeyAdapter = new MyKeyAdapter();
-        addKeyListener(myKeyAdapter);
+        mdot = new MovingDot(new Point(250,30), new Point(x, y), 3 );
         ms = new ArrayList<>();
-        ms.add(new Mountain(new Point(0,500), new Point(100, 250), new Point(200, 500)));
+        System.out.println("working");
+        generateMountains();
+        System.out.println("working2");
         Thread t = new Thread(new ballThread(mdot));
         t.start();
+    }
+
+    public void generateMountains(){
+        int numLeft = safeZone/50+1;
+        System.out.println(numLeft);
+        int numRight = (safeZone+100)/50+1;
+        System.out.println(numRight);
+        int leftCorner = 0;
+        int rightCorner = 100;
+        for (int i=0; i<numLeft+1; i++){
+            //System.out.println(i);
+            int height = (int) (Math.random()*100) + 250;
+            if(rightCorner <= safeZone )
+            {
+                ms.add(new Mountain(new Point(leftCorner,500), new Point((leftCorner+rightCorner)/2, height), new Point(rightCorner, 500)));
+                leftCorner+=50;
+                rightCorner+=50;
+            }
+            else {
+                rightCorner = safeZone;
+                leftCorner = rightCorner - 100;
+            }
+
+        }
+        leftCorner = safeZone+100;
+        rightCorner = leftCorner+100;
+        for (int i=0; i<=numRight+1; i++){
+            //System.out.println("Working");
+            int height = (int) (Math.random()*100) + 250;
+            if(rightCorner <= 500 )
+            {
+                ms.add(new Mountain(new Point(leftCorner,500), new Point((leftCorner+rightCorner)/2, height), new Point(rightCorner, 500)));
+                leftCorner+=50;
+                rightCorner+=50;
+            }
+            else {
+                rightCorner = 500;
+                leftCorner = rightCorner - 100;
+            }
+
+        }
     }
 
     private class ballThread implements Runnable {
@@ -53,7 +98,7 @@ public class LLController extends JPanel {
                 }
                 for (Mountain m:ms){
                     if(m.intersect(md.getRegion())){
-                        System.out.println("Crush");
+                        //System.out.println("Crush");
                     }
                 }
                 //System.out.println("working");
@@ -74,8 +119,9 @@ public class LLController extends JPanel {
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        for(Mountain m: ms)
+        for(Mountain m: ms) {
             m.paint(g);
+        }
         mdot.paint(g);
     }
     private class MyKeyAdapter extends KeyAdapter {
@@ -83,7 +129,7 @@ public class LLController extends JPanel {
         @Override
         public void keyPressed(KeyEvent e) {
             keyCode = e.getKeyCode();
-            repaint();
+            System.out.println("ssss");
         }
 
     }
